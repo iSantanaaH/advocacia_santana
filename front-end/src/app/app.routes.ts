296ab7d7d23aa244fa4fb8e0b186112ccfe0d7d9
Micael Santana <micaelsantana.dev@gmail.com>
2024-05-10 15:33:36 -0300

    create layout home page

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { HomePageLayoutComponent } from './layouts/home-page-layout/home-page-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: HomePageLayoutComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'page-not-found',
+    component: NotFoundComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'page-not-found',
+  },
+];
---
+++ front-end/src/app/layouts/home-page-layout/home-page-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+
+@Component({
+  selector: 'app-home-page-layout',
+  standalone: true,
+  imports: [HeaderComponent, FooterComponent],
+  templateUrl: './home-page-layout.component.html',
+  styleUrl: './home-page-layout.component.css',
+})
+export class HomePageLayoutComponent {}
