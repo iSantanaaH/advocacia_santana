64139d6b303f52e18c8ac87ebc6ff87a677d22d0
Micael Santana <micaelsantana.dev@gmail.com>
2024-05-20 18:54:02 -0300

    create alertCircle.svg, show and hidden alert according to the inputs valid our touched using directive [ngClass], create simpple validate form using FormBuilder, Validators and ReactiveFormsModule

--- front-end/src/app/pages/register/register.component.ts
+++ front-end/src/app/pages/register/register.component.ts
@@ -1,12 +1,23 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgClass],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  constructor(private _fb: FormBuilder) {}
 
+  public registerForm = this._fb.group({
+    name: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+    repeatPassword: ['', Validators.required],
+    birthdate: ['', [Validators.required, Validators.maxLength(8)]],
+    phone: ['', [Validators.required, Validators.maxLength(11)]],
+  });
 }
