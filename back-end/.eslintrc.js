396b7cb6d67c27cd09e20cecbe8b1fa4761e052d
Micael Santana <micaelsantana.dev@gmail.com>
2024-05-25 10:06:53 -0300

    create options for CORS to accept request for the domain localhost:4200, edit config for formatt code in prettier and eslint, install prisma

--- back-end/.eslintrc.js
+++ back-end/.eslintrc.js
@@ -10,10 +10,10 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
-  root: true,
+  root: false,
   env: {
-    node: true,
-    jest: true,
+    node: false,
+    jest: false,
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
@@ -21,5 +21,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
   },
 };
--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options: CorsOptions = {
+    origin: 'http://localhost:4200',
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+    allowedHeaders: 'Content-type, Autorization',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  };
+  app.enableCors(options);
+
   await app.listen(3000);
 }
 bootstrap();
