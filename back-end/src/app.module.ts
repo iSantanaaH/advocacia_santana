b2f2a40a2127c603c8a592385167e830f73b3594
Micael Santana <micaelsantana.dev@gmail.com>
2024-05-25 10:09:20 -0300

    import services and controllers

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RegisterController } from './user/controllers/register/register.controller';
+import { LoginController } from './user/controllers/login/login.controller';
+import { RegisterService } from './user/services/register/register.service';
+import { LoginService } from './user/services/login/login.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, RegisterController, LoginController],
+  providers: [AppService, RegisterService, LoginService],
 })
 export class AppModule {}
