939f3ae090453965ee27f23b07299c7244d56444
Micael Santana <micaelsantana.dev@gmail.com>
2024-06-06 20:56:13 -0300

    create and export interface LoginResponse, this is model receive for the back-end. change url in variable apiUrl. Create method login and return observable and send email and password for the back-end

--- front-end/src/app/services/user/login/login.service.ts
+++ front-end/src/app/services/user/login/login.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface LoginResponse {
+  token: string;
+  user: {
+    id: number;
+    name: string;
+  };
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoginService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  private apiUrl = 'http://localhost:3000/api/auth-user/login';
+
+  login(email: string, password: string): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>(this.apiUrl, { email, password });
+  }
 }
