42031a429cbd77c395867290a83faa40c973bcc7
Micael Santana <micaelsantana.dev@gmail.com>
2024-06-07 22:12:30 -0300

    create change auth.guard.ts to use isLoggedIn inside auth.service that verify is authenticated user

--- front-end/src/app/services/user/auth/authGuard/auth.guard.ts
+++ front-end/src/app/services/user/auth/authGuard/auth.guard.ts
@@ -1,5 +1,15 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../authService/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (authService.isLoggedIn()) {
+    router.navigate(['/']);
+    return false;
+  } else {
+    return true;
+  }
 };
