9d81678278c0cca52ac2976b06330d1cf0e2a269
iSantanaaH <micaelsantana.dev@gmail.com>
2024-07-29 18:54:18 -0300

    add animation active and disable menu burguer

--- front-end/src/app/pages/home/home-page-layout.component.ts
+++ front-end/src/app/pages/home/home-page-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home-page-layout',
@@ -8,9 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-page-layout.component.css', './home-page-responsive.css'],
 })
 export default class HomePageLayoutComponent {
+  @ViewChild('menuHidden') menuHidden!: ElementRef;
   public showMenu: boolean = false;
 
   toggleMenu(): void {
     this.showMenu = !this.showMenu;
+  }
+
+  disableMenu(): void {
+    this.menuHidden.nativeElement.classList.add('disableMenu');
+    setTimeout(() => {
+      this.showMenu = false;
+    }, 1000);
   }
 }
