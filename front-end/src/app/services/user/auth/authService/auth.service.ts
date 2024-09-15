a8b026ed25fda43534a2b736c3a9288e94e7498b
iSantanaaH <micaelsantana.dev@gmail.com>
2024-09-14 21:32:59 -0300

    Create variable userName to receive userName in login.component.ts. Create function getToken to find token auth in cookies. Update isLoggedIn to use function getToken. Add param userName in function login to receive userName in login.component.ts. Update function logout, set userName = null after logout user. getUserName function created to export userName out component

--- front-end/src/app/services/user/auth/authService/auth.service.ts
+++ front-end/src/app/services/user/auth/authService/auth.service.ts
@@ -6,29 +6,39 @@ import { CookieService } from '../cookie/cookie.service';
 })
 export class AuthService {
   private readonly COOKIE_NAME = 'a7fK9Lz0jH';
+  private userName: string | null = null;
 
   constructor(private cookieService: CookieService) {}
 
   isLoggedIn(): boolean {
+    const token = this.getToken();
+    return !!token;
+  }
+
+  private getToken(): string | null {
     if (typeof window !== 'undefined') {
       const cookies = document.cookie.split(';');
       for (let i = 0; i < cookies.length; i++) {
-        const cookie = cookies[i];
-
-        if (cookie.startsWith(this.COOKIE_NAME)) {
-          const token = cookie;
-          return token !== null;
+        const cookie = cookies[i].trim();
+        if (cookie.startsWith(`${this.COOKIE_NAME}`)) {
+          return cookie.split('=')[1];
         }
       }
     }
-    return false;
+    return null;
   }
 
-  login(token: string): void {
+  login(token: string, userName: string): void {
     this.cookieService.setCookie(this.COOKIE_NAME, token, 1);
+    this.userName = userName;
   }
 
   logout(): void {
     this.cookieService.deleteCookie(this.COOKIE_NAME);
+    this.userName = null;
+  }
+
+  getUserName(): string | null {
+    return this.userName;
   }
 }
