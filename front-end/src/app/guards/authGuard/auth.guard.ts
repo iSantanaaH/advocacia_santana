27de616c91e3b1dd22d2f35556838d69180b85c0
iSantanaaH <micaelsantana.dev@gmail.com>
2024-09-16 20:29:42 -0300

    create folder guards and move authGuard inside services to inside guards folder

---
+++ front-end/src/app/guards/authGuard/auth.guard.ts
@@ -0,0 +1,15 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../../services/user/auth/authService/auth.service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (authService.isLoggedIn()) {
+    router.navigate(['/']);
+    return false;
+  } else {
+    return true;
+  }
+};
