db453b2e03abb8ee93134a25ed287ca80657b230
iSantanaaH <micaelsantana.dev@gmail.com>
2024-09-16 19:37:27 -0300

    create page postCreator

---
+++ front-end/src/app/pages/postCreator/post-creator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-creator',
+  standalone: true,
+  imports: [],
+  templateUrl: './post-creator.component.html',
+  styleUrl: './post-creator.component.css'
+})
+export class PostCreatorComponent {
+
+}
