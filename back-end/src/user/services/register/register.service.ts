34f06bb8c74305a055d1e4e266649cc8fecaa2d0
iSantanaaH <micaelsantana.dev@gmail.com>
2024-09-26 16:06:10 -0300

    Import BadRequestException. Create verify to existing user by name or email and send error message for the controller

--- back-end/src/user/services/register/register.service.ts
+++ back-end/src/user/services/register/register.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/services/prisma/prisma.service';
 import { UserDataProps } from './userDataProps';
 import { User } from '@prisma/client';
@@ -8,6 +8,20 @@ export class RegisterService {
   constructor(private prisma: PrismaService) {}
 
   async register(userData: UserDataProps): Promise<User> {
+    const existingUser = await this.prisma.user.findFirst({
+      where: {
+        OR: [{ name: userData.name }, { email: userData.email }],
+      },
+    });
+
+    if (existingUser) {
+      if (userData.name === existingUser.name) {
+        throw new BadRequestException('Já existe um usuário com esse nome');
+      } else if (userData.email === existingUser.email) {
+        throw new BadRequestException('Esse emai já está sendo usado');
+      }
+    }
+
     const user = await this.prisma.user.create({
       data: {
         name: userData.name,
