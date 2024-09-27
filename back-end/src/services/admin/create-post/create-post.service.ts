fbf48c036d80d37bd1e1794c7a747a95d35f3414
iSantanaaH <micaelsantana.dev@gmail.com>
2024-09-27 20:35:14 -0300

    create function postCreator to connect with database and create post

--- back-end/src/services/admin/create-post/create-post.service.ts
+++ back-end/src/services/admin/create-post/create-post.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { CreatePostDataModel } from 'src/controllers/admin/create-post/createPostDataModel';
+import { PrismaService } from 'src/services/prisma/prisma.service';
 
 @Injectable()
-export class CreatePostService {}
+export class CreatePostService {
+  constructor(private prisma: PrismaService) {}
+
+  async postCreator(postData: CreatePostDataModel) {
+    const post = await this.prisma.post.create({
+      data: {
+        title: postData.title,
+        description: postData.description,
+        image: postData.image,
+        author: {
+          connect: { id: postData.userId },
+        },
+      },
+    });
+    return post;
+  }
+}
