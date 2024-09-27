17f537b8f51896379b9d51a11bab5f88c5b3b224
iSantanaaH <micaelsantana.dev@gmail.com>
2024-09-27 20:37:41 -0300

    config controller to receive request and use createPostService

--- back-end/src/controllers/admin/create-post/create-post.controller.ts
+++ back-end/src/controllers/admin/create-post/create-post.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { CreatePostService } from 'src/services/admin/create-post/create-post.service';
+import { CreatePostDataModel } from './createPostDataModel';
 
-@Controller('create-post')
-export class CreatePostController {}
+@Controller('api/admin/createPost')
+export class CreatePostController {
+  constructor(private readonly createPostService: CreatePostService) {}
+
+  @Post()
+  @HttpCode(200)
+  async createPost(@Body() postData: CreatePostDataModel) {
+    if (postData) {
+      try {
+        const postResult = await this.createPostService.postCreator(postData);
+        console.log(postResult);
+        return postResult;
+      } catch (error) {
+        throw new UnauthorizedException(error.message);
+      }
+    } else {
+      throw new BadRequestException('Preencha todos os campos!');
+    }
+  }
+}
