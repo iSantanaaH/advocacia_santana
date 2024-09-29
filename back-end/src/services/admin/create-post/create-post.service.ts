fdf8f865e4f4d943cf265be4eb955423293056fc
iSantanaaH <micaelsantana.dev@gmail.com>
2024-09-29 17:36:47 -0300

    update postCreator, add param imageName and imagePath receive from controller to insert in database

--- back-end/src/services/admin/create-post/create-post.service.ts
+++ back-end/src/services/admin/create-post/create-post.service.ts
@@ -6,17 +6,23 @@ import { PrismaService } from 'src/services/prisma/prisma.service';
 export class CreatePostService {
   constructor(private prisma: PrismaService) {}
 
-  async postCreator(postData: CreatePostDataModel) {
+  async postCreator(
+    postData: CreatePostDataModel,
+    imageName: string,
+    imagePath: string,
+  ) {
     const post = await this.prisma.post.create({
       data: {
         title: postData.title,
         description: postData.description,
-        image: postData.image,
+        image_name: imageName,
+        image_path: imagePath,
         author: {
           connect: { id: postData.userId },
         },
       },
     });
+    console.log(`Post criado com sucesso! ${JSON.stringify(post)}`);
     return post;
   }
 }
