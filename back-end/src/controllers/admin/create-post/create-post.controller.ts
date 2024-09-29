0c59eef57e10cf5e5b1910fc7d0e687fcdcd6ccd
iSantanaaH <micaelsantana.dev@gmail.com>
2024-09-29 17:31:50 -0300

    configure controller to accept file comming to front-end

--- back-end/src/controllers/admin/create-post/create-post.controller.ts
+++ back-end/src/controllers/admin/create-post/create-post.controller.ts
@@ -5,10 +5,14 @@ import {
   Controller,
   HttpCode,
   Post,
-  UnauthorizedException,
+  UploadedFile,
+  UseInterceptors,
 } from '@nestjs/common';
 import { CreatePostService } from 'src/services/admin/create-post/create-post.service';
 import { CreatePostDataModel } from './createPostDataModel';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 @Controller('api/admin/createPost')
 export class CreatePostController {
@@ -16,14 +20,44 @@ export class CreatePostController {
 
   @Post()
   @HttpCode(200)
-  async createPost(@Body() postData: CreatePostDataModel) {
-    if (postData) {
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: './uploads/admin/posts',
+        filename: (req, file, callback) => {
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const ext = extname(file.originalname);
+          const filename = `${file.fieldname}-${uniqueSuffix}${ext}`;
+          callback(null, filename);
+        },
+      }),
+    }),
+  )
+  async createPost(
+    @Body() postData: CreatePostDataModel,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    const userId = Number(postData.userId);
+    postData.userId = userId;
+    console.log(postData);
+    if (
+      !postData.title ||
+      !postData.description ||
+      !postData.userId ||
+      !postData.image
+    ) {
       try {
-        const postResult = await this.createPostService.postCreator(postData);
-        console.log(postResult);
+        const imagePath = file.path;
+        const imageName = file.filename;
+        const postResult = await this.createPostService.postCreator(
+          postData,
+          imageName,
+          imagePath,
+        );
         return postResult;
       } catch (error) {
-        throw new UnauthorizedException(error.message);
+        throw new BadRequestException(error.message);
       }
     } else {
       throw new BadRequestException('Preencha todos os campos!');
