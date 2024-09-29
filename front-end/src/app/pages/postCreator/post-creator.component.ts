7051e200211875c6bbf4c7746afef0dc0d8622f4
iSantanaaH <micaelsantana.dev@gmail.com>
2024-09-29 17:41:40 -0300

    add inject authService and creted function getUserIdToken to extract user.id in token using functions inside authService to extract and decode token. Change type image from string for null. Take first index in variable image. Use FormData to send object image for the api and create subscription

--- front-end/src/app/pages/postCreator/post-creator.component.ts
+++ front-end/src/app/pages/postCreator/post-creator.component.ts
@@ -3,9 +3,9 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PostCreatorService } from '../../services/postCreator/post-creator.service';
 import { ToastComponent } from '../../components/toast/toast/toast.component';
 import { Subscription } from 'rxjs';
-import { error } from 'console';
 import { HttpResponse } from '@angular/common/http';
 import { CreatePostResponse } from '../../services/postCreator/createPostResponse';
+import { AuthService } from '../../services/user/auth/authService/auth.service';
 
 @Component({
   selector: 'app-post-creator',
@@ -20,39 +20,70 @@ import { CreatePostResponse } from '../../services/postCreator/createPostRespons
 export default class PostCreatorComponent implements OnDestroy {
   constructor(
     private _fb: FormBuilder,
-    private postCreatorService: PostCreatorService
+    private postCreatorService: PostCreatorService,
+    private authService: AuthService
   ) {}
 
   @ViewChild(ToastComponent) toastComponent!: ToastComponent;
 
   public TOAST_MESSAGE: string = '';
   private subscription: Subscription = new Subscription();
+  SELECTED_FILE: File | null = null;
 
   public createPostForm = this._fb.group({
     title: ['', Validators.required],
     description: ['', Validators.required],
-    image: ['', Validators.required],
+    image: [null, Validators.required],
   });
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    if (file) {
+      this.SELECTED_FILE = file;
+    }
+  }
+
+  getUserIdToken() {
+    const token = this.authService.getToken();
+    if (token) {
+      const decodedToken = this.authService.decodedToken(token);
+      const userId = decodedToken.user.id;
+      return userId;
+    }
+  }
 
   onSubmit() {
     if (this.createPostForm.valid) {
       const title = this.createPostForm.get('title')?.value ?? '';
       const description = this.createPostForm.get('description')?.value ?? '';
-      const image = this.createPostForm.get('image')?.value ?? '';
+      const image = this.createPostForm.get('image')?.value?.[0];
 
-      const createPostSubscription = this.postCreatorService
-        .createPost(title, description, image)
-        .subscribe({
-          next: (response: HttpResponse<CreatePostResponse>) => {
-            if (response.status === 200) {
-              const responseBody = response.body;
-              console.log(responseBody);
-            }
-          },
-          error: (error) => {},
-        });
+      if (image) {
+        const formData = new FormData();
+        formData.append('title', title);
+        formData.append('description', description);
+        formData.append('userId', this.getUserIdToken());
 
-      console.log('válido');
+        if (this.SELECTED_FILE) {
+          formData.append('image', this.SELECTED_FILE);
+        }
+
+        const createPostSub = this.postCreatorService
+          .createPost(formData)
+          .subscribe({
+            next: (response: HttpResponse<CreatePostResponse>) => {
+              if (response.status === 200) {
+                const responseBody = response.body;
+              }
+            },
+            error: (error) => {},
+          });
+        this.subscription.add(createPostSub);
+      } else {
+        this.TOAST_MESSAGE = `Por favor, selecione uma imagem`;
+        this.toastComponent.message = this.TOAST_MESSAGE;
+        this.toastComponent.showToast();
+      }
     } else {
       this.TOAST_MESSAGE = `Preencha todos os campos`;
       this.toastComponent.message = this.TOAST_MESSAGE;
