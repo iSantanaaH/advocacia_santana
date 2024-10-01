816ba0db56e0551f44988a0edb83de4ca4a87714
iSantanaaH <micaelsantana.dev@gmail.com>
2024-10-01 12:25:00 -0300

    create component published and unpublished to call components inside father page manage-post

---
+++ front-end/src/app/pages/admin/published/published.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-published',
+  standalone: true,
+  imports: [],
+  templateUrl: './published.component.html',
+  styleUrls: ['./published.component.css'],
+})
+export class PublishedComponent {}
---
+++ front-end/src/app/pages/admin/unpublished/unpublished.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-unpublished',
+  standalone: true,
+  imports: [],
+  templateUrl: './unpublished.component.html',
+  styleUrls: ['./unpublished.component.css'],
+})
+export class UnpublishedComponent {}
