1d57016c168eadc7b303c9aaf206ebea92a7586f
iSantanaaH <micaelsantana.dev@gmail.com>
2024-10-02 17:43:29 -0300

    create logic to guard routes admin with user.roleId

--- front-end/src/app/guards/roleGuard/role.guard.ts
+++ front-end/src/app/guards/roleGuard/role.guard.ts
@@ -1,5 +1,16 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../../services/user/auth/authService/auth.service';
 
-export const roleGuard: CanActivateFn = (route, state) => {
+export const roleGuard: CanActivateFn = async (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  const UserRoleId = await authService.getUserRole();
+
+  if (UserRoleId != 1) {
+    router.navigate(['']);
+    return false;
+  }
+
   return true;
 };
