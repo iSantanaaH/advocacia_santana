7020fd8cc42a4aec582d3c5faa4a2a6f70b17d0c
iSantanaaH <micaelsantana.dev@gmail.com>
2024-10-03 20:44:22 -0300

    remove async and await in roleGuard

--- front-end/src/app/guards/roleGuard/role.guard.ts
+++ front-end/src/app/guards/roleGuard/role.guard.ts
@@ -2,10 +2,10 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../../services/user/auth/authService/auth.service';
 
-export const roleGuard: CanActivateFn = async (route, state) => {
+export const roleGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
-  const UserRoleId = await authService.getUserRole();
+  const UserRoleId = authService.getUserRole();
 
   if (UserRoleId != 1) {
     router.navigate(['']);
