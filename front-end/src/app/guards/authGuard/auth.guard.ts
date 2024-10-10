036e5efbbcaa6816e315bb5552414fc4888684b0
iSantanaaH <micaelsantana.dev@gmail.com>
2024-10-10 07:04:16 -0300

    return original state

--- front-end/src/app/guards/authGuard/auth.guard.ts
+++ front-end/src/app/guards/authGuard/auth.guard.ts
@@ -6,8 +6,10 @@ export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  if (authService.isLoggedIn()) {
-    router.navigate(['/']);
+  const isAuthenticated = authService.isLoggedIn();
+
+  if (isAuthenticated) {
+    router.navigate(['']);
     return false;
   }
 
