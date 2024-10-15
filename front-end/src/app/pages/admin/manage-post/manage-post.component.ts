5743dfd6f3e3859b7da550290362d126019cbfd5
iSantanaaH <micaelsantana.dev@gmail.com>
2024-10-15 19:31:15 -0300

    import component showUser to use in template

--- front-end/src/app/pages/admin/manage-post/manage-post.component.ts
+++ front-end/src/app/pages/admin/manage-post/manage-post.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ShowUserComponent } from '../../../components/user/showUser/show-user.component';
 
 @Component({
   selector: 'app-manage-post',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ShowUserComponent],
   templateUrl: './manage-post.component.html',
   styleUrl: './manage-post.component.css',
 })
