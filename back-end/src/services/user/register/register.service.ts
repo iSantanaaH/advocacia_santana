7cab67b7d4ed11676865071b683dfd74b948142c
Micael Santana <micaelsantana.dev@gmail.com>
2024-12-25 17:20:19 -0300

    use function hashService inside HashService to hash password before save in database

--- back-end/src/services/user/register/register.service.ts
+++ back-end/src/services/user/register/register.service.ts
@@ -2,10 +2,14 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/services/utils/prisma/prisma.service';
 import { UserDataProps } from './userDataProps';
 import { User } from '@prisma/client';
+import { HashService } from 'src/services/utils/hash-service/hash-service';
 
 @Injectable()
 export class RegisterService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private readonly hashService: HashService,
+  ) {}
 
   async register(userData: UserDataProps): Promise<User> {
     const existingUser = await this.prisma.user.findFirst({
@@ -18,15 +22,17 @@ export class RegisterService {
       if (userData.name === existingUser.name) {
         throw new BadRequestException('Já existe um usuário com esse nome');
       } else if (userData.email === existingUser.email) {
-        throw new BadRequestException('Esse emai já está sendo usado');
+        throw new BadRequestException('Esse email já está sendo usado');
       }
     }
+
+    const hashPassword = await this.hashService.hashPassword(userData.password);
 
     const user = await this.prisma.user.create({
       data: {
         name: userData.name,
         email: userData.email,
-        password: userData.password,
+        password: hashPassword,
         birthdate: userData.birthdate,
         phone: userData.phone,
         roleId: 2,
