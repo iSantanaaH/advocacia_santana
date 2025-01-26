81314bfc8a84a0b415bd4ed9144c1aaedd0280cd
Micael Santana <micaelsantana.dev@gmail.com>
2025-01-26 15:55:30 -0300

    add AuthService in construtor to use function logout in template

--- front-end/src/app/pages/admin/manage-post/manage-post.component.ts
+++ front-end/src/app/pages/admin/manage-post/manage-post.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ShowUserComponent } from '../../../components/user/showUser/show-user.component';
+import { AuthService } from '../../../services/user/auth/authService/auth.service';
 
 @Component({
   selector: 'app-manage-post',
@@ -12,4 +13,6 @@ import { ShowUserComponent } from '../../../components/user/showUser/show-user.c
     './manage-post-responsive.component.css',
   ],
 })
-export default class ManagePostComponent {}
+export default class ManagePostComponent {
+  constructor(public readonly authService: AuthService) {}
+}
