a78ff173a01de53d9d7f3414ed2f2e1b71a945d2
Micael Santana <micaelsantana.dev@gmail.com>
2025-01-26 16:38:32 -0300

    remove getHello

--- back-end/src/app.service.ts
+++ back-end/src/app.service.ts
@@ -1,10 +1,4 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
-  getHello() {
-    return {
-      message: 'Hello World!',
-    };
-  }
-}
+export class AppService {}
