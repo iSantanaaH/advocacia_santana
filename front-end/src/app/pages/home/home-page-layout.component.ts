f4fffd0b9896ef37adc69165a42a27bddc46e395
iSantanaaH <micaelsantana.dev@gmail.com>
2025-05-04 13:23:29 -0300

    remove template and styles header from home and import header-component

--- front-end/src/app/pages/home/home-page-layout.component.ts
+++ front-end/src/app/pages/home/home-page-layout.component.ts
@@ -1,12 +1,13 @@
 import { NgOptimizedImage } from '@angular/common';
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { AuthService } from '../../services/user/auth/authService/auth.service';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-home-page-layout',
   standalone: true,
-  imports: [NgOptimizedImage, FormsModule],
+  imports: [NgOptimizedImage, FormsModule, HeaderComponent],
   templateUrl: './home-page-layout.component.html',
   styleUrls: ['./home-page-layout.component.css', './home-page-responsive.css'],
 })
@@ -14,20 +15,4 @@ export default class HomePageLayoutComponent {
   constructor(public authService: AuthService) {
     authService.getUserRole();
   }
-  @ViewChild('menuHidden') menuHidden!: ElementRef;
-  public showMenu: boolean = false;
-  public searchQuery: string = '';
-
-  toggleMenu(): void {
-    this.showMenu = !this.showMenu;
-  }
-
-  disableMenu(): void {
-    this.menuHidden.nativeElement.classList.add('disableMenu');
-    setTimeout(() => {
-      this.showMenu = false;
-    }, 700);
-  }
-
-  onSearch() {}
 }
