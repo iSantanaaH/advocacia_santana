44a43db88c6ac1c7449c20bc3f48b3c2e826ba1b
iSantanaaH <micaelsantana.dev@gmail.com>
2025-05-13 00:44:15 -0300

    add parameters to the url to tell the api which posts to fetch

--- front-end/src/app/services/posts/unpublished/unpublished.service.ts
+++ front-end/src/app/services/posts/unpublished/unpublished.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UnPublishedDataModel } from './unPublishedDataModel';
@@ -13,8 +13,11 @@ export class UnpublishedService {
     'http://localhost:3000/api/admin/manage-posts/unpublished-posts';
 
   getUnpublishedPosts(): Observable<HttpResponse<UnPublishedDataModel[]>> {
+    const params = new HttpParams().set('unpublished', 'false');
+
     return this.http.get<UnPublishedDataModel[]>(this.apiUrl, {
       observe: 'response',
+      params,
     });
   }
 }
