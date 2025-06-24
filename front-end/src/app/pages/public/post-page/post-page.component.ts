de7deb63964d967e5f1f793df597046e6f0f50c4
iSantanaaH <micaelsantana.dev@gmail.com>
2025-06-23 22:18:27 -0300

    create post-page

---
+++ front-end/src/app/pages/public/post-page/post-page.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PostService } from '../../../services/admin/post/post.service';
+import { PostModel } from '../../../services/admin/post/postModel';
+import { NgOptimizedImage } from '@angular/common';
+
+@Component({
+  selector: 'app-post-page',
+  templateUrl: './post-page.component.html',
+  styleUrls: ['./post-page.component.css'],
+  imports: [NgOptimizedImage],
+  standalone: true,
+})
+export default class PostPageComponent implements OnInit {
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostService
+  ) {}
+
+  post!: PostModel;
+
+  ngOnInit(): void {
+    const postId = Number(this.route.snapshot.paramMap.get('id'));
+    this.postService.getPostById(postId).subscribe({
+      next: (data) => (this.post = data),
+      error: (err) => console.error('Erro ao buscar post', err),
+    });
+  }
+}
