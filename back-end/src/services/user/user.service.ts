bee8e653ebb0430b07d84816653af54f3cb07e7d
iSantanaaH <micaelsantana.dev@gmail.com>
2025-06-24 18:18:41 -0300

    add comment in function

--- back-end/src/services/user/user.service.ts
+++ back-end/src/services/user/user.service.ts
@@ -5,6 +5,8 @@ dotenv.config({ path: '../../../.env' });
 import { PrismaService } from '../utils/prisma/prisma.service';
 import { HashService } from '../utils/hash-service/hash-service';
 import { LoginModel } from 'src/models/user/LoginModel';
+import { CadastroModel } from 'src/models/user/CadastroModel';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class UserService {
@@ -13,6 +15,39 @@ export class UserService {
     private readonly hashService: HashService,
   ) {}
 
+  // Faz o cadastro do usuário;
+  async register(userData: CadastroModel): Promise<User> {
+    const existingUser = await this.prisma.user.findFirst({
+      where: {
+        OR: [{ name: userData.name }, { email: userData.email }],
+      },
+    });
+
+    if (existingUser) {
+      if (userData.name === existingUser.name) {
+        throw new BadRequestException('Já existe um usuário com esse nome');
+      } else if (userData.email === existingUser.email) {
+        throw new BadRequestException('Esse email já está sendo usado');
+      }
+    }
+
+    const hashPassword = await this.hashService.hashPassword(userData.password);
+
+    const user = await this.prisma.user.create({
+      data: {
+        name: userData.name,
+        email: userData.email,
+        password: hashPassword,
+        birthdate: userData.birthdate,
+        phone: userData.phone,
+        roleId: 2,
+        status: true,
+      },
+    });
+    return user;
+  }
+
+  // Faz o login do usuário;
   async login(userData: LoginModel) {
     const user = await this.prisma.user.findUnique({
       where: {
