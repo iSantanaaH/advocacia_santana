77a30c15b39cd65703d5a20c378c7d35e2fbc942
iSantanaaH <micaelsantana.dev@gmail.com>
2025-06-26 11:22:23 -0300

    formatt code

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -7,11 +7,25 @@ import { HashService } from './services/utils/hash-service/hash-service';
 import { PostController } from './controllers/admin/post/post.controller';
 import { PostService } from './services/admin/post/post.service';
 import { UserController } from './controllers/user/user.controller';
-import { UserService } from './services/user/user.service';
+import { UserService } from './services/public/user/user.service';
+import { PostUserController } from './controllers/user/post/post.user.controller';
+import { PostUserService } from './services/public/user/post/post.user.service';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [AppController, PostController, UserController],
-  providers: [AppService, PrismaService, HashService, PostService, UserService],
+  controllers: [
+    AppController,
+    PostController,
+    UserController,
+    PostUserController,
+  ],
+  providers: [
+    AppService,
+    PrismaService,
+    HashService,
+    PostService,
+    UserService,
+    PostUserService,
+  ],
 })
 export class AppModule {}
