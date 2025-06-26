25d74f0c342e5fa51f98aed944218124a0f7aacb
iSantanaaH <micaelsantana.dev@gmail.com>
2025-06-26 11:52:59 -0300

    create function to mount url the api and update all functions to use function buildUrl in requisition

--- front-end/src/app/services/admin/post/post.service.ts
+++ front-end/src/app/services/admin/post/post.service.ts
@@ -11,33 +11,51 @@ import { UnpublishedModel } from '../../../models/posts/unpublishedPostModel';
 export class PostService {
   constructor(private http: HttpClient) {}
 
-  private readonly apiUrl = 'http://localhost:3000/api/admin/manage-post/post';
+  private readonly apiUrl = 'http://localhost:3000/api';
 
-  getPublishedPosts(): Observable<PostModel[]> {
-    const params = new HttpParams().set('published', 'true');
-    return this.http.get<PostModel[]>(this.apiUrl, { params });
+  // Montar Url dinâmica confome a função;
+  private buildUrl(scope: 'admin' | 'public', path: string): string {
+    return `${this.apiUrl}/${scope}/${path}`;
   }
 
-  getPostById(id: number): Observable<PostModel> {
-    return this.http.get<PostModel>(`${this.apiUrl}/${id}`);
-  }
-
-  getUnpublishedPosts(): Observable<UnpublishedModel[]> {
-    const params = new HttpParams().set('published', 'false');
-    return this.http.get<PostModel[]>(this.apiUrl, { params });
-  }
-
-  publishPost(id: string): Observable<PostModel> {
-    return this.http.patch<PostModel>(`${this.apiUrl}/${id}/publish`, {});
-  }
-
-  deletePost(id: string): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`);
-  }
-
+  // Admin;
   createPost(formData: FormData): Observable<HttpResponse<CreatePostModel>> {
     return this.http.post<CreatePostModel>(this.apiUrl, formData, {
       observe: 'response',
     });
+  }
+
+  getPublishedPosts(): Observable<PostModel[]> {
+    const params = new HttpParams().set('published', 'true');
+    return this.http.get<PostModel[]>(this.buildUrl('admin', 'manage-post'), {
+      params,
+    });
+  }
+
+  getUnpublishedPosts(): Observable<UnpublishedModel[]> {
+    const params = new HttpParams().set('published', 'false');
+    return this.http.get<PostModel[]>(this.buildUrl('admin', 'manage-post'), {
+      params,
+    });
+  }
+
+  publishPost(id: string): Observable<PostModel> {
+    return this.http.patch<PostModel>(
+      `${this.buildUrl('admin', 'manage-post')}/publish/${id}`,
+      {}
+    );
+  }
+
+  deletePost(id: string): Observable<void> {
+    return this.http.delete<void>(
+      `${this.buildUrl('admin', 'manage-post')}/delete/${id}`
+    );
+  }
+
+  // Public;
+  getPostById(id: number): Observable<PostModel> {
+    return this.http.get<PostModel>(
+      `${this.buildUrl('public', 'post-details')}/${id}`
+    );
   }
 }
