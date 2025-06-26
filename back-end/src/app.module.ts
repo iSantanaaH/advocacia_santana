1910668bbd2007115530d62f4369b9b33cbca827
iSantanaaH <micaelsantana.dev@gmail.com>
2025-06-26 11:25:45 -0300

    formatt code

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -6,9 +6,9 @@ import { PrismaModule } from './services/utils/prisma/prisma.module';
 import { HashService } from './services/utils/hash-service/hash-service';
 import { PostController } from './controllers/admin/post/post.controller';
 import { PostService } from './services/admin/post/post.service';
-import { UserController } from './controllers/user/user.controller';
+import { UserController } from './controllers/public/user/user.controller';
 import { UserService } from './services/public/user/user.service';
-import { PostUserController } from './controllers/user/post/post.user.controller';
+import { PostUserController } from './controllers/public/user/post/post.user.controller';
 import { PostUserService } from './services/public/user/post/post.user.service';
 
 @Module({
