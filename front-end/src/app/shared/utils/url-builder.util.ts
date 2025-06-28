7393f128df1813a3aa59e175047974029ffacc29
iSantanaaH <micaelsantana.dev@gmail.com>
2025-06-28 19:36:20 -0300

    create publicApiUrl and adminApiUrl

--- front-end/src/app/shared/utils/url-builder.util.ts
+++ front-end/src/app/shared/utils/url-builder.util.ts
@@ -1,5 +1,9 @@
 const API_BASE_URL = 'http://localhost:3000/api';
 
-export function buildApiUrl(scope: 'admin' | 'public', path: string): string {
-  return `${API_BASE_URL}/${scope}/${path}`;
+export function adminApiUrl(path: string): string {
+  return `${API_BASE_URL}/admin/${path}`;
+}
+
+export function publicApiUrl(path: string): string {
+  return `${API_BASE_URL}/${path}`;
 }
