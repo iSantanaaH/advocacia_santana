fcc56c288ade15d81acc5a26dd24a1b48366545c
iSantanaaH <micaelsantana.dev@gmail.com>
2025-06-28 13:02:51 -0300

    remove apiUrl and import and use buildApiUrl in service to mount url

--- front-end/src/app/services/user/auth/login/login.service.ts
+++ front-end/src/app/services/user/auth/login/login.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { buildApiUrl } from '../../../../shared/utils/url-builder.util';
 
 export interface LoginResponse {
   token: string;
@@ -12,14 +13,12 @@ export interface LoginResponse {
 export class LoginService {
   constructor(private http: HttpClient) {}
 
-  private apiUrl = 'http://localhost:3000/api/auth/login';
-
   login(
     email: String,
     password: String
   ): Observable<HttpResponse<LoginResponse>> {
     return this.http.post<LoginResponse>(
-      this.apiUrl,
+      buildApiUrl('public', 'user/authentication'),
       { email, password },
       { observe: 'response' }
     );
