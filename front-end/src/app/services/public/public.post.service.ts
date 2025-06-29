bb2d405437104ac30da671f9cb67458f7192b26b
iSantanaaH <micaelsantana.dev@gmail.com>
2025-06-28 22:19:09 -0300

    move functions public inside service

--- front-end/src/app/services/public/public.post.service.ts
+++ front-end/src/app/services/public/public.post.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PostResponse } from '../../models/posts/post-response.model';
+import { HttpClient } from '@angular/common/http';
+import { publicApiUrl } from '../../shared/utils/url-builder.util';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PublicPostService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getPublishedPosts(): Observable<PostResponse[]> {
+    return this.http.get<PostResponse[]>(publicApiUrl('posts/publisheds'));
+  }
+
+  getPostDetailsById(id: number): Observable<PostResponse> {
+    return this.http.get<PostResponse>(`${publicApiUrl('post-details')}/${id}`);
+  }
 }
