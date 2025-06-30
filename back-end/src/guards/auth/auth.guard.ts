bce5ebe745836df686320b0e9c01de59a15f5ac5
iSantanaaH <micaelsantana.dev@gmail.com>
2025-06-30 19:52:37 -0300

    create logic to auth user to guard routes with authentication

--- back-end/src/guards/auth/auth.guard.ts
+++ back-end/src/guards/auth/auth.guard.ts
@@ -1,11 +1,34 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Request } from 'express';
+import * as jwt from 'jsonwebtoken';
+import { UserFromToken } from 'src/models/public/user/user-from-token.model';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest<Request>();
+    const authHeader = request.headers['authorization'];
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      throw new UnauthorizedException('Token ausente ou mal formatado');
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+      const decoded = jwt.verify(
+        token,
+        process.env.SECRET_KEY,
+      ) as UserFromToken;
+      request.user = decoded;
+      return true;
+    } catch (error) {
+      throw new UnauthorizedException('Token inválido');
+    }
   }
 }
